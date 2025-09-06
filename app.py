c81c12ef97485c333078e8fdc0b7165bea092678
Ahmad Babatunde <ahmadtijani1639@gmail.com>
2025-09-06 17:36:14 +0100

    edit cors origin

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def create_app() -> FastAPI:
     # Add CORS middleware
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["*", "http://localhost:3000", "https://esticore.vercel.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
